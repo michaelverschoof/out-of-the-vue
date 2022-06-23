bab093ae8874a65ec7ec3d470ff1cf879e65c7a4
Michael Verschoof <m.verschoof@btcdirect.eu>
2022-06-23 14:57:40 +0200

    Added Vue plugin capability and renamed the css file

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
-/**
- * Full components
- */
-export { default as Modal } from './components/modal.vue';
-export { default as CheckableField } from './components/form/fields/checkable-field/checkable-field.vue';
-export { default as TextField } from './components/form/fields/input-field/text-field.vue';
-export { default as NumberField } from './components/form/fields/input-field/number-field.vue';
-export { default as OneTimeCodeField } from './components/form/fields/input-field/one-time-code-field.vue';
-
-/**
- * Base field components
- */
-export { default as CheckableInput } from './components/form/fields/base/checkable-input.vue';
-export { default as TextInput } from './components/form/fields/base/text-input.vue';
-export { default as NumericInput } from './components/form/fields/base/numeric-input.vue';
-export { default as DebounceableInput } from './components/form/fields/base/debounceable-input.vue';
-export { default as ValidatableInput } from './components/form/fields/base/validatable-input.vue';
+import { App } from 'vue';
 
 /**
  * Base component additions
  */
-export { default as Counter } from './components/form/fields/additions/counter/counter.vue';
-export { default as TextCounter } from './components/form/fields/additions/counter/text-counter.vue';
-export { default as PrependAppend } from './components/form/fields/additions/layout/prepend-append.vue';
+import Counter from './components/form/fields/additions/counter/counter.vue';
+import TextCounter from './components/form/fields/additions/counter/text-counter.vue';
+import PrependAppend from './components/form/fields/additions/layout/prepend-append.vue';
+
+/**
+ * Base components
+ */
+import CheckableInput from './components/form/fields/base/checkable-input.vue';
+import DebounceableInput from './components/form/fields/base/debounceable-input.vue';
+import NumericInput from './components/form/fields/base/numeric-input.vue';
+import TextInput from './components/form/fields/base/text-input.vue';
+import ValidatableInput from './components/form/fields/base/validatable-input.vue';
+
+/**
+ * Full components
+ */
+import CheckableField from './components/form/fields/checkable-field/checkable-field.vue';
+import NumberField from './components/form/fields/input-field/number-field.vue';
+import OneTimeCodeField from './components/form/fields/input-field/one-time-code-field.vue';
+import TextField from './components/form/fields/input-field/text-field.vue';
+import Modal from './components/modal.vue';
+
+/**
+ * Component exports
+ */
+export { CheckableField, Modal, NumberField, OneTimeCodeField, TextField };
+export { CheckableInput, DebounceableInput, NumericInput, TextInput, ValidatableInput };
+export { Counter, PrependAppend, TextCounter };
 
 /**
  * Composables
@@ -34,3 +43,23 @@ export { predefinedValidations, useUserInputValidation } from './composables/val
  * Symbols
  */
 export { SubmittedSymbol } from './composables/types';
+
+export default {
+    install: (app: App) => {
+        app.component('Counter', Counter);
+        app.component('TextCounter', TextCounter);
+        app.component('PrependAppend', PrependAppend);
+
+        app.component('CheckableInput', CheckableInput);
+        app.component('DebounceableInput', DebounceableInput);
+        app.component('NumericInput', NumericInput);
+        app.component('TextInput', TextInput);
+        app.component('ValidatableInput', ValidatableInput);
+
+        app.component('CheckableField', CheckableField);
+        app.component('Modal', Modal);
+        app.component('NumberField', NumberField);
+        app.component('OneTimeCodeField', OneTimeCodeField);
+        app.component('TextField', TextField);
+    }
+};
