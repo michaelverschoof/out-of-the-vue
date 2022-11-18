900502b8a337b1594d1607ccbd551f49f14b26ff
Michael Verschoof <m.verschoof@btcdirect.eu>
2022-11-18 14:33:53 +0100

    Added text/number field stories

---
+++ src/components/form/fields/input-field/number-field.story.vue
@@ -0,0 +1,179 @@
+<template>
+    <story title="Components/Input field/Number field" :layout="{ type: 'grid', width: 400 }" auto-props-disabled>
+        <variant title="Simple">
+            <number-field
+                name="histoire-text-field-simple"
+                :value="state.value"
+                :typing-delay="state.delay"
+                :allow-decimals="true"
+                :allow-negative="true"
+                required
+                @created="logEvent('created', $event)"
+                @updated="update"
+            />
+        </variant>
+
+        <variant title="With label and information">
+            <number-field
+                name="histoire-text-field-labels"
+                :value="state.value"
+                :allow-decimals="true"
+                :allow-negative="true"
+                required
+                @created="logEvent('created', $event)"
+                @updated="update"
+            >
+                <template #label>Field label</template>
+                <template #information><i>Helpful text on how to fill in the field</i></template>
+            </number-field>
+        </variant>
+
+        <variant title="With prepend and append">
+            <number-field
+                name="histoire-text-field-prepend-append"
+                :value="state.value"
+                :allow-decimals="true"
+                :allow-negative="true"
+                required
+                @created="logEvent('created', $event)"
+                @updated="update"
+            >
+                <template #label>Field label</template>
+                <template #information>Helpful text on how to fill in the field</template>
+                <template #prepend><icon icon="mdi:airballoon" /></template>
+                <template #append><icon icon="mdi:airplane" /></template>
+            </number-field>
+        </variant>
+
+        <variant title="Inside another prepend and append for outer icons">
+            <prepend-append>
+                <number-field
+                    name="histoire-text-field-nested-prepend-append"
+                    :value="state.value"
+                    :allow-decimals="true"
+                    :allow-negative="true"
+                    @created="logEvent('created', $event)"
+                    @updated="update"
+                >
+                    <template #label>Field label</template>
+                    <template #information>Helpful text on how to fill in the field</template>
+                    <template #prepend><icon icon="mdi:airballoon" /></template>
+                    <template #append><icon icon="mdi:airplane" /></template>
+                </number-field>
+                <template #prepend><icon icon="mdi:airballoon" width="3rem" /></template>
+                <template #append><icon icon="mdi:airplane" width="3rem" /></template>
+            </prepend-append>
+        </variant>
+
+        <variant title="With validations">
+            <template #default>
+                <number-field
+                    name="histoire-text-field-validations"
+                    :value="state.value"
+                    :allow-decimals="true"
+                    :allow-negative="true"
+                    :min="state.min"
+                    :max="state.max"
+                    required
+                    @created="logEvent('created', $event)"
+                    @updated="update"
+                >
+                    <template #label>Field label</template>
+                    <template #information>Helpful text on how to fill in the field</template>
+                    <template #required>Required error message</template>
+                    <template #min>Minimum amount error message</template>
+                    <template #max>Maximum amount error message</template>
+                </number-field>
+            </template>
+
+            <template #controls>
+                <hst-slider v-model="state.min" :step="100" :min="0" :max="1000" title="Minimum value" />
+                <hst-slider v-model="state.max" :step="100" :min="1000" :max="2000" title="Maximum value" />
+
+                <label class="histoire-wrapper htw-p-2 htw-flex htw-gap-2 htw-flex-wrap htw-items-center">
+                    <span class="htw-w-28 htw-shrink-0">Validate field</span>
+                    <span class="htw-grow htw-max-w-full htw-flex htw-items-center htw-gap-1">
+                        <hst-button class="htw-p-2" @click="validate = !validate"> Validate = {{ validate }}</hst-button>
+                    </span>
+                </label>
+            </template>
+        </variant>
+
+        <template #controls>
+            <show-grid-lines show />
+
+            <hst-slider v-model="state.delay" :step="100" :min="0" :max="1000" title="Typing delay" />
+
+            <h3>Validations</h3>
+            <hst-checkbox v-model="state.required" title="Required field" />
+
+            <hst-checkbox v-model="state.allowDecimals" title="Allow decimals" />
+            <label class="histoire-wrapper htw-p-2 htw-flex htw-gap-2 htw-flex-wrap">
+                <span class="htw-w-28 htw-shrink-0" />
+                <span class="htw-grow htw-max-w-full htw-flex htw-items-center htw-gap-1">
+                    <hst-button :disabled="!state.allowDecimals" class="htw-p-2" @click="toggleDecimal"> Toggle decimal </hst-button>
+                </span>
+            </label>
+
+            <hst-checkbox v-model="state.allowNegative" title="Allow negative numbers" />
+            <label class="histoire-wrapper htw-p-2 htw-flex htw-gap-2 htw-flex-wrap">
+                <span class="htw-w-28 htw-shrink-0" />
+                <span class="htw-grow htw-max-w-full htw-flex htw-items-center htw-gap-1">
+                    <hst-button :disabled="!state.allowNegative" class="htw-p-2" @click="toggleMinus"> Toggle minus sign </hst-button>
+                </span>
+            </label>
+        </template>
+    </story>
+</template>
+
+<script lang="ts" setup>
+import { SubmittedSymbol } from '@/composables/types';
+import { Icon } from '@iconify/vue';
+import { logEvent } from 'histoire/client';
+import { provide, reactive, ref } from 'vue';
+import ShowGridLines from '../../../../../test/components/show-grid-lines.vue';
+import { toggleDecimalInValue, toggleMinusInValue } from '../../../../../test/functions/numbers';
+import PrependAppend from '../../../layout/prepend-append.vue';
+import NumberField from './number-field.vue';
+
+const validate = ref<boolean>(false);
+provide(SubmittedSymbol, validate);
+
+const state = reactive({
+    value: 1234.56,
+    delay: 200,
+    allowDecimals: true,
+    allowNegative: true,
+    required: true,
+    min: 200,
+    max: 1200
+});
+
+const previousDecimal = ref<number>(null);
+const toggleDecimal = () => {
+    const { value, index } = toggleDecimalInValue(state.value, previousDecimal.value);
+    state.value = value;
+    previousDecimal.value = index;
+};
+
+const toggleMinus = () => {
+    state.value = toggleMinusInValue(state.value);
+};
+
+const update = (event: any) => {
+    state.value = event.value;
+    logEvent('updated', event);
+};
+</script>
+
+<docs lang="md">
+# Text field
+
+The text field provides a structured way of building form fields.
+This field allows for validations with pre-built validations enabled by default.
+It also contains the structure to add a label above the input,
+hint information below the input, error messages and prepended/appended icons.
+All of these can be any text or HTML that you want so you can build the field how you want it.
+</docs>
+
+<style scoped></style>
---
+++ src/components/form/fields/input-field/text-field.story.vue
@@ -0,0 +1,82 @@
+<template>
+    <story title="Components/Input field/Text field" :layout="{ type: 'grid', width: 400 }" auto-props-disabled>
+        <variant title="Simple">
+            <text-field
+                name="histoire-text-field"
+                :value="state.value"
+                :typing-delay="state.delay"
+                @created="logEvent('created', $event)"
+                @updated="logEvent('updated', $event)"
+            />
+        </variant>
+
+        <variant title="With label and information">
+            <text-field name="histoire-text-field-2" :value="state.value" @created="logEvent('created', $event)" @updated="logEvent('updated', $event)">
+                <template #label>Field label</template>
+                <template #information><i>Helpful text on how to fill in the field</i></template>
+            </text-field>
+        </variant>
+
+        <variant title="With prepend and append">
+            <text-field name="histoire-text-field-3" :value="state.value" @created="logEvent('created', $event)" @updated="logEvent('updated', $event)">
+                <template #label>Field label</template>
+                <template #information>Helpful text on how to fill in the field</template>
+                <template #prepend><icon icon="mdi:airballoon" /></template>
+                <template #append><icon icon="mdi:airplane" /></template>
+            </text-field>
+        </variant>
+
+        <variant title="Inside another prepend and append for outer icons">
+            <prepend-append>
+                <text-field name="histoire-text-field-3" :value="state.value" @created="logEvent('created', $event)" @updated="logEvent('updated', $event)">
+                    <template #label>Field label</template>
+                    <template #information>Helpful text on how to fill in the field</template>
+                    <template #prepend><icon icon="mdi:airballoon" /></template>
+                    <template #append><icon icon="mdi:airplane" /></template>
+                </text-field>
+                <template #prepend><icon icon="mdi:airballoon" width="3rem" /></template>
+                <template #append><icon icon="mdi:airplane" width="3rem" /></template>
+            </prepend-append>
+        </variant>
+
+        <template #controls>
+            <show-grid-lines show />
+
+            <hst-slider v-model="state.delay" :step="100" :min="0" :max="1000" title="Typing delay" />
+
+            <h3>Validations</h3>
+            <hst-checkbox v-model="state.required" title="Required field" />
+            <hst-slider v-model="state.min" :step="1" :min="0" :max="20" title="Minimum length" />
+            <hst-slider v-model="state.max" :step="1" :min="0" :max="20" title="Maximum length" />
+        </template>
+    </story>
+</template>
+
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue';
+import { logEvent } from 'histoire/client';
+import { reactive } from 'vue';
+import ShowGridLines from '../../../../../test/components/show-grid-lines.vue';
+import PrependAppend from '../../../layout/prepend-append.vue';
+import TextField from './text-field.vue';
+
+const state = reactive({
+    value: 'Some value',
+    delay: 200,
+    required: true,
+    min: 3,
+    max: 15
+});
+</script>
+
+<docs lang="md">
+# Text field
+
+The text field provides a structured way of building form fields.
+This field allows for validations with pre-built validations enabled by default.
+It also contains the structure to add a label above the input,
+hint information below the input, error messages and prepended/appended icons.
+All of these can be any text or HTML that you want so you can build the field how you want it.
+</docs>
+
+<style scoped></style>
